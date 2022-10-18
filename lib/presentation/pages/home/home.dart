e0835b31567d064dd2a682017614f0f6d27e9080
Maxim Pervushin <sekyret1@yandex.ru>
2022-10-19 00:57:57 +0300

    add selectable

--- lib/presentation/pages/home/home.dart
+++ lib/presentation/pages/home/home.dart
@@ -3,7 +3,6 @@ import 'package:web_lab/gen/assets.gen.dart';
 import 'package:web_lab/presentation/pages/home/widgets/article.dart';
 import 'package:web_lab/presentation/pages/home/widgets/mail_input_form.dart';
 import 'package:web_lab/presentation/pages/home/widgets/our_team.dart';
-import 'package:web_lab/presentation/pages/home/widgets/recent_card.dart';
 import 'package:web_lab/presentation/styles/colors.dart';
 import 'package:web_lab/presentation/styles/typography.dart';
 import 'package:web_lab/presentation/widgets/app_outlined_button.dart';
@@ -22,58 +21,62 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.white,
-      body: Column(
-        children: [
-          HomeAppBar(),
-          Expanded(
-            child: ListView(
-              children: [
-                const Header(),
-                const GetInTouch(),
-                const SizedBox(height: 79),
-                Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    'HOW WE MAKE BRANDS THRIVE',
-                    style: AppTypography.m.s22.w400.black51,
+      body: SelectableRegion(
+        focusNode: FocusNode(),
+        selectionControls: MaterialTextSelectionControls(),
+        child: Column(
+          children: [
+            HomeAppBar(),
+            Expanded(
+              child: ListView(
+                children: [
+                  const Header(),
+                  const GetInTouch(),
+                  const SizedBox(height: 79),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      'HOW WE MAKE BRANDS THRIVE',
+                      style: AppTypography.m.s22.w400.black51,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 73),
-                const Thrives(),
-                const SizedBox(height: 74),
-                const RecentProjects(),
-                const OurTeam(),
-                const Article(),
-                const LetsGetInTouch(),
-                ColoredBox(
-                  color: AppColors.black0,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const SizedBox(height: 69),
-                      MailInputForm(),
-                      const SizedBox(height: 21.95),
-                      AppOutlinedButton(
-                        text: 'Send message',
-                        onTap: () {},
-                        borderColor: AppColors.green,
-                        textStyle: AppTypography.v.italic.s16.green,
-                      ),
-                      const SizedBox(height: 37.5),
-                      Assets.images.logoWhite.image(),
-                      const SizedBox(height: 14),
-                      Text(
-                        'Minimal Template made in Webflow. All rights reserved.',
-                        style: AppTypography.v.s14.w400.textTransGrey,
-                      ),
-                      const SizedBox(height: 75),
-                    ],
+                  const SizedBox(height: 73),
+                  const Thrives(),
+                  const SizedBox(height: 74),
+                  const RecentProjects(),
+                  const OurTeam(),
+                  const Article(),
+                  const LetsGetInTouch(),
+                  ColoredBox(
+                    color: AppColors.black0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 69),
+                        MailInputForm(),
+                        const SizedBox(height: 21.95),
+                        AppOutlinedButton(
+                          text: 'Send message',
+                          onTap: () {},
+                          borderColor: AppColors.green,
+                          textStyle: AppTypography.v.italic.s16.green,
+                        ),
+                        const SizedBox(height: 37.5),
+                        Assets.images.logoWhite.image(),
+                        const SizedBox(height: 14),
+                        Text(
+                          'Minimal Template made in Webflow. All rights reserved.',
+                          style: AppTypography.v.s14.w400.textTransGrey,
+                        ),
+                        const SizedBox(height: 75),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
